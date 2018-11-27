da871d7e77d316f60f166be1c9e40dbfffc0bc5d
Naenae L <oneprincesslea@gmail.com>
2018-11-27 19:38:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ function currentLine(katzDeliLine){
     } else {
         var text = "The line is currently: "; 
         for (i=0; i++; i<katzDeliLine.length){
+          console.log(katzDeliLine[i]);
           text = `${text} ${katzDeliLine[i]}` ; 
         } 
         return text; 
