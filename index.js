8b1c4db047d380e5c97ee5111359914ebf250a5d
Naenae L <oneprincesslea@gmail.com>
2018-11-27 19:40:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,8 +23,7 @@ function currentLine(katzDeliLine){
         return "The line is currently empty."; 
     } else {
         var text = "The line is currently: "; 
-        for (i=0; i<katzDeliLine.length; i++){
-
+        for (i=0; i<katzDeliLine.length; i++){ 
           text = `${text} ${katzDeliLine[i]}` ; 
         } 
         return text; 
